90aed0b5d660e2e19eea77bd89b1c67cca311248
Rafis Shigapov <rafis1work@gmail.com>
2025-10-13 19:59:48 +0300

    change minor routing behavior

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { HeaderContainer, HeaderBlock, ThemeHeaderBtn } from './Header.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleThemeAction } from '../../feature/themeList';
@@ -9,6 +9,7 @@ import { RootState } from '../../store';
 export const Header = () => {
   const theme = useSelector((state: RootState) => state.themeList.theme);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const getActiveClass = ({ isActive }: { isActive: boolean }): string => {
     return isActive ? 'active link' : 'link';
@@ -21,12 +22,21 @@ export const Header = () => {
     transform: 'translateY(-50%)',
   };
 
+  const handleGoHome = (e: React.MouseEvent) => {
+    e.preventDefault();
+    navigate('/', { replace: true }); // заменяет текущий маршрут, не добавляя в историю
+  };
+
   return (
     <HeaderBlock>
       <HeaderContainer>
-        <NavLink to="/" className={getActiveClass}>
+        <a
+          href="/"
+          className={getActiveClass({ isActive: location.pathname === '/' })}
+          onClick={handleGoHome}
+        >
           Главная
-        </NavLink>
+        </a>
         <NavLink to="/list" className={getActiveClass}>
           Список
         </NavLink>
